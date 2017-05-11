c5d5a43fc613d2a6a225414246588951b831e865
Yraya Bolanos Frasquet <yraya.bf@gmail.com>
2017-05-11 13:36:11 +0100

    Delete and modify implemented. LocalStorage added
    Now it's able to delete and modify the alumns list, which is saved and
    loaded using LocalStorage

--- js/controllers/main.controller.js
+++ js/controllers/main.controller.js
@@ -4,17 +4,23 @@
     angular.module('app_name')
         .controller('AlumnController', alumnController);
 
-    function alumnController($scope) {
+    function alumnController($scope, $http) {
         $scope.alumns = [];
 
         $scope.addAlumn = addAlumn;
         $scope.resetForm = resetForm;
         $scope.modify = modify;
+        $scope.applyModification = applyModification;
+        $scope.deleteAlumn = deleteAlumn;
+        $scope.myFilter = myFilter;
 
         function addAlumn() {
-            $scope.alumns.push($scope.newAlumn);
-            save();
-            resetForm();
+            checkImageExits().then(function () {
+                $scope.newAlumn.id = generateID();
+                $scope.alumns.push($scope.newAlumn);
+                save();
+                resetForm();
+            });
         }
 
         function save() {
@@ -24,17 +30,79 @@
         function load() {
             $scope.alumns = JSON.parse(localStorage.getItem("alumnsData"));
         }
-        
+
+        function modify(alumn) {
+            $scope.newAlumn = angular.copy(alumn);
+            $scope.modifying = true;
+        }
+
+        function applyModification() {
+            checkImageExits().then(function () {
+                var index = getAlumnPosition($scope.newAlumn.id);
+                if (index != -1)
+                    $scope.alumns[index] = $scope.newAlumn;
+                save();
+                resetForm();
+            });
+        }
+
+        function getAlumnPosition(alumnID) {
+            for (var i = 0; i < $scope.alumns.length; i++) {
+                if ($scope.alumns[i].id === alumnID) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        function generateID() {
+            return Math.random().toString(36).substr(2, 10);
+        }
 
         function resetForm() {
             $scope.newAlumn = {};
+            $scope.modifying = false;
+        }
+
+        function deleteAlumn(alumn) {
+            if (confirm("Are you sure you want to delete '" + alumn.name + "'?")) {
+                var index = getAlumnPosition(alumn.id);
+                $scope.alumns.splice(index, 1);
+                save();
+            }
+        }
+
+        function myFilter(alumn) {
+            if (alumn[$scope.option].includes($scope.search))
+                return true;
+            return false;
         }
 
         function init() {
             if (localStorage.getItem("alumnsData")) {
                 load();
             }
+            $scope.modifying = false;
         }
+
+        function resetAlumns() {
+            $scope.alumns = [];
+            localStorage.removeItem()("alumnsData");
+        };
+
+        function checkImageExits() {
+            var imageUrl = $scope.newAlumn.photoUrl;
+            console.log(imageUrl);
+            return $http.get(imageUrl)
+                .then(
+                    function (response) {
+                        console.log(response);
+                    },
+                    function (response) {
+                        $scope.newAlumn.photoUrl = "images/image-not-found.svg";
+                    });
+        }
+
 
         init();
     }
